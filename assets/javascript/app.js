268c672c3c50afdc7ac33113b63e8aa2ba8af937
Mystic5523 <Mystic5523@hotmail.com>
2018-06-28 21:09:54 -0500

    Fin

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,3 @@
-var nextTrain;
-var minAway;
-console.log("Hello");
-
 $(document).ready(function () {
     $("#add").click(function () {
         event.preventDefault();
@@ -9,9 +5,34 @@ $(document).ready(function () {
         var dest = $("#dest").val();
         var startTime = $("#startTime").val();
         var freq = $("#freq").val();
-        var markup = "<tr><th>" + tName + "</th><td>" + dest + "</td><td>" + freq + "</td><td>" + nextTrain + "</td><td>" + minAway + "</td></tr>";
-        $("table tbody").append(markup);
-        console.log(tName, dest, startTime, freq)
+        var firstTimeConverted = moment(startTime, "HH:mm").subtract(1, "years");
+        var currentTime = moment();
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        var tRemainder = diffTime % freq;
+        var minAway = freq - tRemainder;
+
+        var nextTrain = moment().add(minAway, "minutes");
+        var next = moment(nextTrain).format("HH:mm")
+        console.log(minAway)
+        console.log(next)
+
+        console.log("Hello");
+
+        tName = $("#tName").val().trim();
+        dest = $("#dest").val().trim();
+        startTime = $("#startTime").val().trim();
+        freq = $("#freq").val().trim();
+
+
+
+        database.ref().push({
+            tName: tName,
+            dest: dest,
+            startTime: startTime,
+            freq: freq,
+            trainTime: next,
+            nextTrain: minAway
+        });
     });
 });
 var config = {
@@ -21,8 +42,8 @@ var config = {
     projectId: "train-tracker-63938",
     storageBucket: "train-tracker-63938.appspot.com",
     messagingSenderId: "292879422217"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
@@ -32,30 +53,12 @@ var dest = "";
 var startTime = "";
 var freq = "";
 
-$("#add").on("click", function (event) {
-    event.preventDefault();
 
-    tName = $("#tName").val().trim();
-    dest = $("#dest").val().trim();
-    startTime = $("#startTime").val().trim();
-    freq = $("#freq").val().trim();
-
-    database.ref().push({
-        tName: tName,
-        dest: dest,
-        startTime: startTime,
-        freq: freq
-    });
-});
 database.ref().on("child_added", function (snapshot) {
     var sv = snapshot.val();
 
-    console.log(sv.tName);
-    console.log(sv.dest);
-    console.log(sv.startTime);
-    console.log(sv.freq);
-    var nextTrain = moment().diff(moment(sv.startTime), 'months');
-    $("table tbody").append("<tr><th>" + sv.tName + "</th><td>" + sv.dest + "</td><td>" + sv.startTime + "</td><td>" + nextTrain + "</td><td>" + sv.freq + "</td></tr>");
+    console.log(sv.minAway)
+    $("table tbody").append("<tr><th>" + sv.tName + "</th><td>" + sv.dest + "</td><td>" + sv.freq + "</td><td>" + sv.trainTime + "</td><td>" + sv.nextTrain + "</td></tr>");
 
 
 })
